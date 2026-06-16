323ade09dfe7901898c6f07ee3fa6cf88c767867
Fisher Brown <fish.brown17@gmail.com>
2026-06-15 22:49:21 -0400

    add Javadoc comment to CalculatorTest.java

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -2,6 +2,7 @@ package com.leszko.calculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+/** unit tests for the calculator service*/
 public class CalculatorTest {
      private Calculator calculator = new Calculator();
 
