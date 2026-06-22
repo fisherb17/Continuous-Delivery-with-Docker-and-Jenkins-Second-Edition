eb820f24b9e228a0bdb8720388bd5817da0c28ab
Fisher Brown <fish.brown17@gmail.com>
2026-06-22 19:49:26 -0400

    change to java file to test changeset conditional

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -4,6 +4,7 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /** calculator service providing basic arithmetic operations */
+/** conditional test for exercise 5 */
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
